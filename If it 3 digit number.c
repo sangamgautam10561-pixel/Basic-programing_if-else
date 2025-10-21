5591a6b96f19642954a75c5a6bf65cc730272291
Sangam Gautam <sangamgautam10561@gmail.com>
2025-10-21 13:32:51 +0530

    Create If it 3 digit number.c

---
+++ If it 3 digit number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the number :");
+    scanf("%d",&n);
+    if(n>99&&n<1000)
+    printf("It is a 3 digit number");
+    else {
+    printf("It is not a 3 digit number");
+    }
+    return 0;
+}
+  
+   
